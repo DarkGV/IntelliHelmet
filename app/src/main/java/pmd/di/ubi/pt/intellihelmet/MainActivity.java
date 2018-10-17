69c43f43209b424205398df4a44ba15bc1dcd4d2
carolina-lopes <carolinagalvaolopes@gmail.com>
2018-10-17 19:05:50 +0100

    Project creation

---
+++ app/src/main/java/pmd/di/ubi/pt/intellihelmet/MainActivity.java
@@ -0,0 +1,13 @@
+package pmd.di.ubi.pt.intellihelmet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
