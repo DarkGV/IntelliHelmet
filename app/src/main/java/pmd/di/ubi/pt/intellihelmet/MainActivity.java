850b2b5a7e62fd3133c9d8bc2f90e8f813013589
carolina-lopes <carolinagalvaolopes@gmail.com>
2018-11-30 18:27:47 +0000

    the toolbar it's almost done, only need to change color of one icon

--- app/src/main/java/pmd/di/ubi/pt/intellihelmet/MainActivity.java
+++ app/src/main/java/pmd/di/ubi/pt/intellihelmet/MainActivity.java
@@ -1,13 +1,25 @@
 package pmd.di.ubi.pt.intellihelmet;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // the inflation code goes here
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_config, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
